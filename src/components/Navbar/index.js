c2bfbbdcac6f5db10c768db35a7d338bbd313774
Hardik Khandelwal <hardikkhandelwal500@gmail.com>
2021-07-03 17:19:24 +0530

    [add] navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,8 +1,42 @@
-export default function Navbar(){
+import * as React from "react"
+import styled from "@emotion/styled"
+import { useTheme } from '@emotion/react'
+import { BiSun } from 'react-icons/bi';
+import { BsMoon } from 'react-icons/bs';
+
+import Typography from '../Helper/Typography';
+
+const Wrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+    padding: 30px 15px;
+`
+
+const NightModeBtn = styled.button`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background: none;
+    border: none;
+    min-width: 100px;
+    cursor: pointer;
+`
+
+export default function Navbar({ darkThemeEnabled, toggleDarkTheme }) {
+    const theme = useTheme()
+
     return (
-        <div>
-            <h2>Where in the world?</h2>
-            <button>Dark Mode</button>
-        </div>
+        <Wrapper style={{ background: theme.secondary }}>
+            <Typography variant="h4" fontWeight={600}>Where in the world?</Typography>
+
+            <NightModeBtn onClick={toggleDarkTheme} style={{color: theme.color}}>
+                {
+                    darkThemeEnabled ?
+                        (<><BsMoon /> <Typography variant="h5" fontWeight={600}>Dark Mode</Typography></>)
+                        :
+                        (<><BiSun /> <Typography variant="h5" fontWeight={600}>Light Mode</Typography></>)
+                }
+            </NightModeBtn>
+        </Wrapper>
     )
 }
