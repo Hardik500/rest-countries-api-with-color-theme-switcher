6ed2c88e10f4725aed14177cb14d23ff317923a2
Hardik Khandelwal <hardikkhandelwal500@gmail.com>
2021-07-04 02:10:51 +0530

    [add] filer option in page

---
+++ src/components/SearchBar/index.js
@@ -0,0 +1,43 @@
+import * as React from "react"
+import { useTheme } from '@emotion/react'
+import styled from "@emotion/styled"
+import {BiSearch} from 'react-icons/bi';
+
+const Wrapper = styled.div`
+    display: flex;
+    align-items: center;
+    background: ${props => props.theme.secondary ?? "white"};
+    color: ${props => props.theme.color ?? "white"};
+    border-radius: 5px;
+    padding: 10px 15px;
+    width: 100%;
+    
+    @media (min-width: 1440px) {
+        min-width: 500px;
+    }
+`
+
+const SearchComponent = styled.input`
+    padding: 10px 15px;
+    color: ${props => props.theme.color ?? "white"};
+    background: ${props => props.theme.secondary ?? "white"};
+    border-radius: 5px;
+    border: none;
+    outline: none;
+    font-size: 16px;
+
+    ::placeholder {
+        color: ${props => props.theme.color ?? "white"};
+    }
+`
+
+export default function SearchBar({setSearchText}){
+    const theme = useTheme()
+
+    return (
+        <Wrapper theme={theme}>
+            <BiSearch size={20}/>
+            <SearchComponent theme={theme} onChange={(e) => setSearchText(e.target.value)} type="string" placeholder="Search for a country...." />
+        </Wrapper>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,34 +1,53 @@
 import '../style.css'
 
 import * as React from "react"
+import fetch from 'isomorphic-fetch';
+import useSWR from 'swr'
 import styled from "@emotion/styled"
 
+import SearchBar from '../components/SearchBar';
 import Layout from '../components/Layout';
 import CountryCard from '../components/CountryCard';
 
 const Wrapper = styled.div`
+  margin: 20px 40px;
+
+  @media (min-width: 1440px) {
+    margin: 20px 100px;
+  }
+`
+
+const FilterWrapper = styled.div`
   display: flex;
-  justify-content: center;
+`
+
+const CountryWrapper = styled.div`
+  display: flex;
   flex-wrap: wrap;
 `
 
+const fetcher = url => fetch(url).then(r => r.json())
+
 const IndexPage = () => {
-  const [countriesData, setCountriesData] = React.useState([]);
+  const [searchText, setSearchText] = React.useState("");
+  const { data: countriesData, error } = useSWR('https://restcountries.eu/rest/v2/all?fields=name;capital;region;population;flag', fetcher)
 
-  React.useEffect(() => {
-    async function fetchData() {
-      const result = await fetch("https://restcountries.eu/rest/v2/all?fields=name;capital;region;population;flag");
-      const data = await result.json()
-      setCountriesData(data);
-    }
-
-    fetchData();
-  }, [])
+  if (error) return "An error has occurred.";
+  if (!countriesData) return "Loading...";
 
   return (
     <Layout>
       <Wrapper>
-      {countriesData.length && countriesData.map((countryData, index) => <CountryCard data={countryData} key={index}/>)}
+        <FilterWrapper>
+          <SearchBar setSearchText={setSearchText}/>
+        </FilterWrapper>
+        <CountryWrapper>
+          {
+            countriesData
+              ?.filter((countryData) => countryData.name.toLocaleLowerCase().includes(searchText.toLocaleLowerCase()))
+              ?.map((countryData, index) => <CountryCard data={countryData} key={index} />)
+          }
+        </CountryWrapper>
       </Wrapper>
     </Layout>
   )
