764462e3c9c4d4272350ceaca644da5eed0fcb6e
Hardik Khandelwal <hardikkhandelwal500@gmail.com>
2021-07-03 17:19:18 +0530

    [add] base layout

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,22 +1,47 @@
 import React from "react"
 import { Global, css } from "@emotion/react"
 import styled from "@emotion/styled"
+import { ThemeProvider } from '@emotion/react'
+
+import Navbar from '../Navbar';
+
+const themeLight = {
+    primary: 'hsl(0, 0%, 98%)',
+    secondary: 'hsl(0, 0%, 100%)',
+    color: 'hsl(200, 15%, 8%)'
+};
+
+const themeDark = {
+    primary: 'hsl(207, 26%, 17%)',
+    secondary: 'hsl(209, 23%, 22%)',
+    color: 'hsl(0, 0%, 100%)'
+};
 
 const Wrapper = styled("div")`
-    border: 2px solid green;
-    padding: 10px;
+    margin: 0;
 `
+
 export default function Layout({ children }) {
+    const [darkThemeEnabled, setDarkTheme] = React.useState(true);
+    const theme = darkThemeEnabled ? themeDark : themeLight;
+
+    const toggleDarkTheme = () => {
+        setDarkTheme(!darkThemeEnabled);
+    }
+
     return (
-        <Wrapper>
-            <Global
-                styles={css`
-                    div {
-                        background: red;
-                        color: white;
-                    }
-            `}/>
-            {children}
-        </Wrapper>
+        <ThemeProvider theme={theme}>
+            <Wrapper>
+                <Global
+                    styles={css`
+                        div {
+                            background: ${theme.primary};
+                            color: ${theme.color};
+                        }
+                `}/>
+                <Navbar darkThemeEnabled={darkThemeEnabled} toggleDarkTheme={toggleDarkTheme}/>
+                {children}
+            </Wrapper>
+        </ThemeProvider>
     )
 }
