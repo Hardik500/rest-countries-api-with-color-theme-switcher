ef8379d77651794072beebdb4454dd880a153e17
Hardik Khandelwal <hardikkhandelwal500@gmail.com>
2021-07-03 17:19:10 +0530

    [add] typography

---
+++ src/components/Helper/Typography/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import cn from "classnames";
+import "./style.css";
+
+const variantsMapping = {
+    h1: "h1",
+    h2: "h2",
+    h3: "h3",
+    h4: "h4",
+    h5: "h5",
+    h6: "h6",
+    subheading1: "h6",
+    subheading2: "h6",
+    body1: "p",
+    body2: "p",
+};
+
+const Typography = ({ variant, color, fontWeight, children, ...props }) => {
+    const Component = variant ? variantsMapping[variant] : "p";
+
+    return (
+        <Component
+            className={cn({
+                [`typography--variant-${variant}`]: variant,
+                [`typography--fontWeight-${fontWeight}`]: fontWeight,
+                [`typography--color-${color}`]: color,
+            })}
+            {...props}
+        >
+            {children}
+        </Component>
+    );
+};
+
+export default Typography;
